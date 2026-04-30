f83dea26404e49c029bb449ad2ead8728cdaf620
Kanishka0709 <kanishkaamrutkar0709@gmail.com>
2026-04-30 11:55:06 +0530

    Update stop.sh

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -2,15 +2,15 @@
 
 echo "===== Stopping FinSmart Application ====="
 
-APP_NAME="FinSmart_Finances-0.0.1-SNAPSHOT.jar"
+APP_NAME="Finsmart_Finances-0.0.1-SNAPSHOT.jar"
 
 PID=$(pgrep -f $APP_NAME)
 
 if [ -z "$PID" ]; then
-    echo "No running application found"
+    echo "No running application found. Skipping stop."
 else
     echo "Stopping application with PID $PID"
     kill -9 $PID
     sleep 5
-    echo "Application stopped"
+    echo "Application stopped."
 fi
